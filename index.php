355d772a7b0e38e98992cc890b6c6b9ce0180dad
GaziYucel <84437883+GaziYucel@users.noreply.github.com>
2021-10-29 15:27:19 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file plugins/generic/optimetaCitation/index.php
+ *
+ * Copyright (c) 2021+ TIB Hannover
+ * Copyright (c) 2021+ Gazi Yucel
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class OptimetaCitationsPlugin
+ * @ingroup plugins_generic_optimetacitation
+ *
+ * @brief Wrapper for the Optimeta Citation plugin.
+ */
+
+require_once('OptimetaCitationsPlugin.inc.php');
+
+return new OptimetaCitationsPlugin();
---
+++ js/submissionWizardForm.js
@@ -0,0 +1,6 @@
+/**
+ * Script submissionWizardForm.js which gets called in the plugin PHP file.
+ * Is used to enable input of citation metadata during article submission.
+ */
+
+//window.alert('submissionWizardForm');
