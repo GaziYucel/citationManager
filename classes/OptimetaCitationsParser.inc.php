8d14d5d62f5ab4bd4b3ee9101eb4ac0c04f8ae50
Gazi Yucel <84437883+GaziYucel@users.noreply.github.com>
2021-12-01 13:27:00 +0100

    OptimetaCitationsParser Class

--- classes/OptimetaCitationsParser.inc.php
+++ classes/OptimetaCitationsParser.inc.php
@@ -15,9 +15,51 @@
 
 class OptimetaCitationsParser
 {
+	/**
+	 * Regex to extract DOI
+	 *
+	 * @var string
+	 */
+	private string $regexDoi = "/^10.\d{4,9}/[-._;()/:A-Z0-9]+$/i";
 
-	function __construct() {
-        // do something usefull
+	/**
+	 * Regex to extract URL's
+	 *
+	 * @var string
+	 */
+	private string $regexUrl = "";
+
+	/**
+	 * Variable which will hold the raw citations
+	 *
+	 * @var string
+	 */
+	private string $citationsRaw = "";
+
+	/**
+	 * Array which hold the parsed citations: i > pid, restRaw
+	 *
+	 * @var array
+	 */
+	private array $parsedCitationsArray = [];
+
+	function __construct(string $citationsRaw = "")
+	{
+		$this->citationsRaw= $citationsRaw;
     }
 
+	/**
+	 * Function which will do the actual parsing and return parsed citations as an array
+	 *
+	 * @return parsedCitationsArray
+	 */
+	public function getParsedCitations() {
+		if(strlen($this->citationsRaw) > 0){
+
+
+
+			return $this->parsedCitationsArray;
+		}
+		return null;
+	}
 }
