262e20bc4046d9f74fb222d56881085fcf65583d
Gazi Yucel <84437883+GaziYucel@users.noreply.github.com>
2022-01-24 09:11:11 +0100

    Github cleanup

--- classes/OptimetaCitationsParser.inc.php
+++ classes/OptimetaCitationsParser.inc.php
@@ -15,187 +15,188 @@
 
 class OptimetaCitationsParser
 {
-    /**
-     * Basis URL of doi
-     *
-     * @var string
-     */
-    private $doiUrl = 'doi.org';
+	/**
+	 * Regex to extract DOI
+	 *
+	 * @var string
+	 */
+	private $regexDoi = '(10[.][0-9]{4,}[^\s"/<>]*/[^\s"<>]+)';
 
-    /**
-     * Regex to extract DOI
-     *
-     * @var string
-     */
-    // '/^10.\d{4,9}/[-._;()/:A-Z0-9]+$/i'; // crossref regex, doesn't work
-    private $regexDoi = '(10[.][0-9]{4,}[^\s"/<>]*/[^\s"<>]+)';
+	/**
+	 * Variable which will hold the raw citations
+	 *
+	 * @var string
+	 */
+	private $citationsRaw = "";
 
-    /**
-     * Variable which will hold the raw citations
-     *
-     * @var string
-     */
-    private $citationsRaw = "";
+	/**
+	 * Array which hold the parsed citations: [ [ "pid" => "pid1", "raw" => "raw1" ], ... ]
+	 *
+	 * @var array
+	 */
+	private $citationsParsed = [];
 
-    /**
-     * Array which hold the parsed citations: [ [ "pid" => "pid1", "raw" => "raw1" ], ... ]
-     *
-     * @var array
-     */
-    private $citationsParsed = [];
+	/**
+	 * Constructor.
+	 * @param $rawCitations string an unparsed citation string
+	 */
+	function __construct(string $citationsRaw = "")
+	{
+		$this->citationsRaw = $citationsRaw;
+	}
 
-    /**
-     * Constructor.
-     * @param $rawCitations string an unparsed citation string
-     */
-    function __construct(string $citationsRaw = "")
-    {
-        $this->citationsRaw= $citationsRaw;
-    }
+	/**
+	 * Returns parsed citations as an array
+	 *
+	 * @return array citationsParsed
+	 */
+	public function getCitationsParsedArray(): array
+	{
+		$this->parse();
 
-    /**
-     * Returns parsed citations as an array
-     *
-     * @return array citationsParsed
-     */
-    public function getCitationsParsedArray(): array
-    {
-        $this->parse();
+		return $this->citationsParsed;
+	}
 
-        return $this->citationsParsed;
-    }
+	/**
+	 * Returns parsed citations as an JSON
+	 *
+	 * @return string (json) citationsParsed
+	 */
+	public function getCitationsParsedJson(): string
+	{
+		$this->parse();
 
-    /**
-     * Returns parsed citations as an JSON
-     *
-     * @return string (json) citationsParsed
-     */
-    public function getCitationsParsedJson(): string
-    {
-        $this->parse();
+		if (sizeof($this->citationsParsed) == 0) {
+			return '[]';
+		}
 
-		if(sizeof($this->citationsParsed) == 0){ return '[]'; }
+		return json_encode($this->citationsParsed);
+	}
 
-        return json_encode($this->citationsParsed);
-    }
+	/**
+	 * Parse and return parsed citations as an array
+	 * Assumed that raw citations are separated with line endings
+	 *
+	 * @param $citationsRaw string
+	 * @return void
+	 */
+	private function parse(): void
+	{
+		// Strip whitespace
+		$citationsRaw = trim($this->citationsRaw);
 
-    /**
-     * Parse and return parsed citations as an array
-     * Assumed that raw citations are separated with line endings
-     *
-     * @param $citationsRaw string
-     * @return void
-     */
-    private function parse(): void
-    {
-        $citationsRaw = $this->citationsRaw;
-        $citationsArray = [];
+		// Strip slashes
+		$citationsRaw = stripslashes($citationsRaw);
 
-        // Strip whitespace
-        $citationsRaw = trim($citationsRaw);
+		// Remove empty lines and normalize line endings.
+		$citationsRaw = preg_replace('/[\r\n]+/s', "\n", $citationsRaw);
 
-        // Strip slashes
-        $citationsRaw = stripslashes($citationsRaw);
+		// Remove trailing/leading line breaks.
+		$citationsRaw = trim($citationsRaw, "\n");
 
-        // Remove empty lines and normalize line endings.
-        $citationsRaw = preg_replace('/[\r\n]+/s', "\n", $citationsRaw);
+		// Return if input is empty
+		if (empty($citationsRaw)) {
+			return;
+		}
 
-        // Remove trailing/leading line breaks.
-        $citationsRaw = trim($citationsRaw, "\n");
+		// input not empty, continue parsing
 
-        // Return if input is empty
-        if(empty($citationsRaw)) { return; }
+		// Break up at line endings.
+		$citationsArray = explode("\n", $citationsRaw);
 
-        // input not empty, continue parsing
+		// Extract DOI from every citation
+		foreach ($citationsArray as $index => $citationRawLine) {
 
-        // Break up at line endings.
-        $citationsArray = explode("\n", $citationsRaw);
+			// Clean single citation
+			$citationRawLine = $this->cleanCitationString($citationRawLine);
 
-        // Extract PID/ DOI from every citation
-        foreach($citationsArray as $index => $citationRawLine) {
+			// Remove numbers from the beginning of each citation
+			$citationRawLine = $this->removeLeadingNumbersFromBeginning($citationRawLine);
 
-            // Clean single citation
-            $citationRawLine = $this->cleanCitationString($citationRawLine);
+			// match doi in citation
+			$this->citationsParsed[] = $this->parseDoi($citationRawLine);
+		}
+	}
 
-            // Remove numbers from the beginning of each citation
-            $citationRawLine = $this->removeLeadingNumbersFromBeginning($citationRawLine);
+	private function parseDoi($citationRawLine): array
+	{
+		$pid = '';
+		$raw = $citationRawLine;
+		$doiArray = [];
+		preg_match($this->regexDoi, $raw, $doiArray);
 
-            // match pid / doi in citation
-            $pid = '';
-            $raw = $citationRawLine;
-            $doiArray = [];
-            preg_match($this->regexDoi, $raw, $doiArray);
+		if (!empty($doiArray[0])) {
+			$pid = $doiArray[0];
+		}
 
-            if(!empty($doiArray[0])) $pid = $doiArray[0];
+		if (!empty($pid)) {
+			$pidCorrect = 'https://doi.org/' . $pid;
 
-            if(!empty($pid))
-            {
-                $raw = str_replace(
-                    'http://' . $this->doiUrl . '/',
-                    'https://' . $this->doiUrl . '/', $raw);
-                $pid = 'https://' . $this->doiUrl . '/' . $pid;
+			$prefix = [
+				'http://doi.org/' . $pid,
+				'http://www.doi.org/' . $pid,
+				'https://www.doi.org/' . $pid,
+				'http://doi:' . $pid,
+				'https://doi:' . $pid,
+				'doi:' . $pid,
+				'doi: ' . $pid
+			];
 
-                if(stristr($raw, $pid)){
-                    $raw = trim(str_replace($pid, '', $raw));
-                }
-                else{
-                    $pid = '';
-                }
-            }
+			$raw = str_replace($prefix, $pidCorrect, $raw);
+			$raw = str_replace($pidCorrect, '', $raw);
+			$raw = str_replace($pid, '', $raw);
+			$raw = $this->cleanCitationString($raw);
 
-            $this->citationsParsed[] = [ "pid" => $pid, "raw" => $raw ];
-        }
+			$pid = trim($pidCorrect, '.');
+		}
 
-        $this->addLog(var_export($this->citationsParsed, true));
+		return ["pid" => $pid, "raw" => $raw];
+	}
 
-    }
+	/**
+	 * Clean and normalize string
+	 *
+	 * @param $citationsRawLine
+	 * @return string
+	 */
+	private function cleanCitationString($citationsRawLine): string
+	{
+		// Strip whitespace
+		$citationsRawLine = trim($citationsRawLine);
 
-    /**
-     * Clean and normalize string
-     *
-     * @param $citationsRawLine
-     * @return string
-     */
-    private function cleanCitationString($citationsRawLine): string
-    {
+		// String .
+		$citationsRawLine = trim($citationsRawLine, '.');
 
-        // Strip whitespace
-        $citationsRawLine = trim($citationsRawLine);
+		// String ,
+		$citationsRawLine = trim($citationsRawLine, ',');
 
-        // Strip slashes
-        $citationsRawLine = stripslashes($citationsRawLine);
+		// Strip slashes
+		$citationsRawLine = stripslashes($citationsRawLine);
 
-        // Remove trailing/leading line breaks.
-        $citationsRawLine = trim($citationsRawLine, "\n");
+		// Remove trailing/leading line breaks.
+		$citationsRawLine = trim($citationsRawLine, "\n");
 
-        // Normalize whitespace
-        $citationsRawLine = preg_replace('/[\s]+/', ' ', $citationsRawLine);
+		// Normalize whitespace
+		$citationsRawLine = preg_replace('/[\s]+/', ' ', $citationsRawLine);
 
-        return $citationsRawLine;
+		return $citationsRawLine;
+	}
 
-    }
+	/**
+	 * Remove numbers from the beginning of each citation.
+	 *
+	 * @param $citationRawLine
+	 * @return string
+	 */
+	private function removeLeadingNumbersFromBeginning($citationRawLine): string
+	{
 
-    /**
-     * Remove numbers from the beginning of each citation.
-     *
-     * @param $citationRawLine
-     * @return string
-     */
-    private function removeLeadingNumbersFromBeginning($citationRawLine): string
-    {
+		$citationRawLine = preg_replace(
+			'/^\s*[\[#]?[0-9]+[.)\]]?\s*/',
+			'',
+			$citationRawLine);
 
-        $citationRawLine = preg_replace(
-            '/^\s*[\[#]?[0-9]+[.)\]]?\s*/',
-            '',
-            $citationRawLine);
+		return $citationRawLine;
 
-        return $citationRawLine;
-
-    }
-
-    /* debug */
-    public $debug = '____S____' . "\r\n";
-    function addLog($str){ $this->debug .= $str . "\r\n"; }
-    function getLog(){ return $this->debug . "____E____"; }
-
+	}
 }
--- js/optimetaCitations.js
+++ js/optimetaCitations.js
@@ -11,3 +11,48 @@
  * @brief Main javascript for the plugin. *
  */
 
+/**
+ *
+ * @returns {string}
+ */
+function optimetaCitationsGetCsrfToken(){
+
+	// ojs version 3.2.1
+	if(typeof $ !== 'undefined' && typeof $.pkp !== 'undefined' && typeof $.pkp.currentUser !== 'undefined' && typeof $.pkp.currentUser.csrfToken !== 'undefined'){
+		return $.pkp.currentUser.csrfToken;
+	}
+
+	// ojs version 3.3.0
+	if(typeof pkp !== 'undefined' && typeof pkp.currentUser !== 'undefined' && typeof pkp.currentUser.csrfToken !== 'undefined'){
+		return pkp.currentUser.csrfToken;
+	}
+
+	return '';
+}
+
+/**
+ *
+ * @param baseArray
+ * @returns {*}
+ */
+function optimetaCitationsGetHelperArray(baseArray){
+	let helperArray = JSON.parse(JSON.stringify(baseArray));
+	for(let i = 0;i < baseArray.length; i++){
+		helperArray[i].editRow = false;
+	}
+	return helperArray;
+}
+
+/**
+ *
+ * @param str
+ * @returns {boolean}
+ */
+function optimetaCitationsIsStringJson(str) {
+	try {
+		JSON.parse(str);
+	} catch (e) {
+		return false;
+	}
+	return true;
+}
