67ed638529240ba6f8616331f16bb73254309984
GaziYucel <84437883+GaziYucel@users.noreply.github.com>
2022-01-25 14:10:56 +0100

    optimetaCitationsFindPathInArray

--- js/optimetaCitations.js
+++ js/optimetaCitations.js
@@ -18,12 +18,15 @@
 function optimetaCitationsGetCsrfToken(){
 
 	// ojs version 3.2.1
-	if(typeof $ !== 'undefined' && typeof $.pkp !== 'undefined' && typeof $.pkp.currentUser !== 'undefined' && typeof $.pkp.currentUser.csrfToken !== 'undefined'){
+	if(typeof $ !== 'undefined' && typeof $.pkp !== 'undefined' &&
+        typeof $.pkp.currentUser !== 'undefined' &&
+        typeof $.pkp.currentUser.csrfToken !== 'undefined'){
 		return $.pkp.currentUser.csrfToken;
 	}
 
 	// ojs version 3.3.0
-	if(typeof pkp !== 'undefined' && typeof pkp.currentUser !== 'undefined' && typeof pkp.currentUser.csrfToken !== 'undefined'){
+	if(typeof pkp !== 'undefined' && typeof pkp.currentUser !== 'undefined' &&
+        typeof pkp.currentUser.csrfToken !== 'undefined'){
 		return pkp.currentUser.csrfToken;
 	}
 
@@ -55,4 +58,49 @@ function optimetaCitationsIsStringJson(str) {
 		return false;
 	}
 	return true;
+}
+
+/**
+ *
+ * @param ob
+ * @param key
+ * @returns {string}
+ */
+function optimetaCitationsFindPathInArray (ob, key) {
+    const path = [];
+    const keyExists = (obj) => {
+        if (!obj || (typeof obj !== "object" && !Array.isArray(obj))) {
+            return false;
+        }
+        else if (obj.hasOwnProperty(key)) {
+            return true;
+        }
+        else if (Array.isArray(obj)) {
+            let parentKey = path.length ? path.pop() : "";
+
+            for (let i = 0; i < obj.length; i++) {
+                path.push(`${parentKey}[${i}]`);
+                const result = keyExists(obj[i], key);
+                if (result) {
+                    return result;
+                }
+                path.pop();
+            }
+        }
+        else {
+            for (const k in obj) {
+                path.push(k);
+                const result = keyExists(obj[k], key);
+                if (result) {
+                    return result;
+                }
+                path.pop();
+            }
+        }
+        return false;
+    };
+
+    keyExists(ob);
+
+    return path.join(".");
 }
